52bbed735fe374bab5a2f0f4c21d177769c47d04
KukolTomas <36823349+KukolTomas@users.noreply.github.com>
2021-02-12 10:13:34 +0100

    Add files via upload

---
+++ Singleton/Singleton/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Interakční logika pro MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = ROD.Text;
+            SingletonTest singleton = SingletonTest.Instance;
+            if (Jmeno.Text == "" || Prijmeni.Text == "" || !DateTime.TryParse(DOB.Text, out DateTime result) || !text.Contains("/") )
+            {
+                MessageBox.Show("Zadejte všechny parametry");
+            }
+            else
+            {
+                singleton.AddToList(Jmeno.Text, Prijmeni.Text, Convert.ToString(result),ROD.Text);
+            }
+        }
+    }
+}
---
+++ Singleton/Singleton/SingletonTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class SingletonTest
+    {
+   
+            static List<string> list;
+            static SingletonTest singleInstance = null;
+            private static readonly object lockingObject = new object();
+            private SingletonTest()
+            {
+                list = new List<string>();
+            }
+            public static SingletonTest Instance
+            {
+                get
+                {
+                    lock (lockingObject)
+                    {
+                        if (singleInstance == null)
+                        {
+                            singleInstance = new SingletonTest();
+                        }
+                    }
+                    return singleInstance;
+                }
+            }
+            public void AddToList(string a, string b, string c, string d)
+            {
+                list.Add(a);
+                list.Add(b);
+                list.Add(c);
+                list.Add(d);
+            }
+        }
+}
